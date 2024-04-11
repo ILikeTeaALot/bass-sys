8a48b113adfa2876b0719618b1bb5b4e4e5ee746
Madeline <madeline@rykan.net>
2024-04-11 10:34:03 +0100

    maintain some degree of compatibility with previous bass-sys api

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,50 @@
+pub use bindings::*;
+#[allow(unused)]
 pub use consts::*;
 pub use functions::*;
 pub use types::*;
 
-mod consts;
-mod functions;
+pub mod functions;
 mod macros;
-pub mod types;
+mod bindings;
+mod dword;
+
+pub use dword::*;
+
+pub mod consts {
+	pub use crate::bindings::*;
+}
+
+pub mod types {
+	use std::ffi::c_char;
+
+	pub use crate::bindings::*;
+
+	pub type BassDeviceInfo = BASS_DEVICEINFO;
+
+	impl BASS_DEVICEINFO {
+		pub fn new(name: *const c_char, driver: *const c_char, flags: DWORD) -> Self {
+			Self {
+				name,
+				driver,
+				flags,
+			}
+		}
+	}
+
+	pub type BassInfo = BASS_INFO;
+
+	pub type BassRecordInfo = BASS_RECORDINFO;
+
+	pub type BassSample = BASS_SAMPLE;
+
+	pub type BassChannelInfo = BASS_CHANNELINFO;
+
+	pub type BassPluginForm = BASS_PLUGINFORM;
+
+	pub type BassPluginInfo = BASS_PLUGININFO;
+
+	pub type Bass3DVector = BASS_3DVECTOR;
+
+	pub type BassFileProcs = BASS_FILEPROCS;
+}
