797628cedec7c96240b0e664f385683ef4c8e654
Madeline <madeline@rykan.net>
2024-04-11 10:32:49 +0100

    macro changes

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,13 @@
 #[macro_export]
 macro_rules! generate_bindings {
-    ($(binding $static_name:ident fn $binding_name:ident($($parameter_name:ident:$parameter_type:ty),*) $(-> $return_type:ty)?;)*) => {
+    {
+	    $library_name:ident;
+		$doc_root:expr;
+	    $(
+	        $(#[$meta:meta])*
+	        fn $binding_name:ident$(<$($generic_type:ident /*$(: $($generic_trait:ty)+)?*/ $(= $generic_default: ty)?),*>)?($($parameter_name:ident$(.$method: ident())*: $parameter_type:ty $(as $parameter_cast: ty)?),*) $(-> $return_type:ty)?;
+	    )*
+    } => {
         $(
             static $static_name: once_cell::sync::Lazy<libloading::Symbol<'static, extern "system" fn($($parameter_name: $parameter_type),*) $(-> $return_type)?>> = once_cell::sync::Lazy::new(|| {
                 if let Ok(function) = unsafe { BASS_LIBRARY.get(stringify!($binding_name).as_bytes()) } {
@@ -16,4 +23,22 @@ macro_rules! generate_bindings {
             }
         )*
     };
+				$(#[$meta])*
+				// #[doc = concat!("The following documentation was directly `include_str!()`'d from the original CHM. Links will not work, and some formatting issues will be present.", include_str!(concat!(env!("OUT_DIR"), $doc_root, stringify!($binding_name), ".html")))]
+				#[doc = include_str!(concat!(env!("OUT_DIR"), $doc_root, stringify!($binding_name), ".md"))]
+				#[allow(non_snake_case)]
+				pub fn $binding_name $(< $($generic_type /*$(: $($generic_trait)+)?*/ $(= $generic_default)?),* >)? ($($parameter_name: $parameter_type),*) $(-> $return_type)? {
+					unsafe { $library_name.$binding_name($($parameter_name$(.$method())* $(as $parameter_cast)?),*) }
+					// [<STATIC_ $binding_name:upper>]($($parameter_name),*)
+				}
+        )*
+    }
+}
+
+
+#[macro_export]
+macro_rules! include_doc {
+	($file:expr $(,)?) => {
+		concat!("The following documentation was directly `include_str!`'d from the original CHM. Links will not work, and some formatting issues will be present.", include_str!($file))
+	};
 }
